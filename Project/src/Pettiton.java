25f7efaad278115ef2267ffd740f844299488cc6
Regan117 <rayburdsal@gmail.com>
2023-10-04 14:46:24 -0400

    Test pt.2

---
+++ Project/src/Pettiton.java
@@ -0,0 +1,8 @@
+package src;
+import java.util.*;
+class petition{
+    public static void main(String[] args){
+    System.out.println("Hello :)");
+    System.out.println("This is dumb");
+    }
+}

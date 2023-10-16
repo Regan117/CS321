9a765a0edac0edb9ab752cc940d0b1dbcda0a63a
Regan117 <rayburdsal@gmail.com>
2023-10-16 14:09:51 -0400

    "added functions"

--- Project/src/UnitTests.java
+++ Project/src/UnitTests.java
@@ -1,5 +1,10 @@
 package src;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.hamcrest.CoreMatchers.*;
 
 public class UnitTests {
-    
+    public void WorkFlowTest1(){
+        
+    }
 }

d32b8477e2d83bd8e691582a6f1b58483ff9834d
Regan117 <rayburdsal@gmail.com>
2023-10-30 15:59:07 -0400

    "worked on petition, made a hashmap for a mockDB, and worked on Approver"

--- sample/src/main/java/org/openjfx/Petition.java
+++ sample/src/main/java/org/openjfx/Petition.java
@@ -1,35 +1,81 @@
 package org.openjfx;
-class petition{
+import java.util.HashMap;
+class Petition{
     protected String Name;
     protected String Email;
     protected int DOB;
-    protected String country;
+    protected String Country;
     protected int Anum;
+    
+    private HashMap<Integer, Petition> dataBaseMap = new HashMap<>();
+
 
     public static void main(String[] args){
     
     }
-    public petition(){
+    public Petition(){
 
     }
-
-    public petition(String name, int dOB, String email, String country, int aNum) {
+    public Petition(String name,int dOB, String email, String country, int anum) {
+        Name = name;
+        Email = email;
+        DOB = dOB;
+        Country = country;
+        Anum = anum;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public String getEmail() {
+        return Email;
+    }
+    public void setEmail(String email) {
+        Email = email;
+    }
+    public int getDOB() {
+        return DOB;
+    }
+    public void setDOB(int dOB) {
+        DOB = dOB;
+    }
+    public String getCountry() {
+        return Country;
+    }
+    public void setCountry(String country) {
+        Country = country;
+    }
+    public int getAnum() {
+        return Anum;
+    }
+    public void setAnum(int anum) {
+        Anum = anum;
+    }
     
+//NOT FINISHED
+    public Boolean validateEntry(String name, int dOB, String email, String country, int aNum) {
+        boolean state =  true;
+        if(!(name instanceof String)){
+            state = false;
+        }
+        return state;
     }
-
-    public Boolean ValidateEntry(String name, int dOB, String email, String country, int aNum) {
-
+    public Boolean searchDB(Petition p){
+        for(Petition i : dataBaseMap.values()) {
+            if(p.equals(i)){
+                return true;
+            }
+        }
         return false;
     }
-    public Boolean SearchDB(petition p){
-
-        return false;
+    public Petition pullFromDB(int ANum){
+        
+        return dataBaseMap.get(Anum);
     }
-    public Boolean PullFromDB(int ANum){
-
-        return false;
-    }
-    public Boolean WriteToDB(petition p){
+    public Boolean writeToDB(Petition p){
+        dataBaseMap.put(Anum, p);
         return true;
     }
 }
--- sample/src/test/java/org/openjfx/PetitionTest.java
+++ sample/src/test/java/org/openjfx/PetitionTest.java
@@ -10,26 +10,26 @@ public class PetitionTest{
 	public String Email = "john@gmail.com";
 	public String Country = "Brazil";
 	public int ANum = 123456789;
-	public petition mockDB[] = new petition[5];
+	public Petition mockDB[] = new Petition[5];
 
-	public final petition p = new petition( name,DOB, Email, Country, ANum);
-	public final petition p1 = new petition( "james bo cane",12341989, Email, Country, 999999999);
+	public final Petition p = new Petition( name,DOB, Email, Country, ANum);
+	public final Petition p1 = new Petition( "james bo cane",12341989, Email, Country, 999999999);
 
 	@Test
 	public void testValidateEntry()throws Exception {
 		//Petition p = new Petition( name,DOB, Email, Country, ANum);
 		/*assertTrue(name instanceof String);
 		assertTrue(p.ValidateEntry(name,DOB,Email,Country,ANum)); */
-		assertEquals(true, p.ValidateEntry(name,DOB,Email,Country,ANum));
-		assertEquals(false, p.ValidateEntry(name,934372849,Email,Country,ANum));
-		assertEquals(false, p.ValidateEntry("123",DOB,Email,Country,ANum));
-		assertEquals(false, p.ValidateEntry(name,DOB,"john",Country,ANum));
-		assertEquals(false, p.ValidateEntry(name,934372849,Email,"UnitedStates",ANum));
-		assertEquals(false, p.ValidateEntry(name,DOB,Email,Country,-2));
-		assertEquals(false, p.ValidateEntry(null,DOB,Email,Country,ANum));
-		assertEquals(false, p.ValidateEntry(name,-2,Email,Country,ANum));
-		assertEquals(false, p.ValidateEntry(name,DOB,null,Country,ANum));
-		assertEquals(false, p.ValidateEntry(name,DOB,Email,null,ANum));
+		assertEquals(true, p.validateEntry(name,DOB,Email,Country,ANum));
+		assertEquals(false, p.validateEntry(name,934372849,Email,Country,ANum));
+		assertEquals(false, p.validateEntry("123",DOB,Email,Country,ANum));
+		assertEquals(false, p.validateEntry(name,DOB,"john",Country,ANum));
+		assertEquals(false, p.validateEntry(name,934372849,Email,"UnitedStates",ANum));
+		assertEquals(false, p.validateEntry(name,DOB,Email,Country,-2));
+		assertEquals(false, p.validateEntry(null,DOB,Email,Country,ANum));
+		assertEquals(false, p.validateEntry(name,-2,Email,Country,ANum));
+		assertEquals(false, p.validateEntry(name,DOB,null,Country,ANum));
+		assertEquals(false, p.validateEntry(name,DOB,Email,null,ANum));
 	}
 
 	@Test
@@ -38,28 +38,29 @@ public class PetitionTest{
 
 		//returns false if petition is not flaged in the database
 		//returns true if petition is not already in the data base
-		assertFalse(p.SearchDB(null));
-		assertFalse( p.SearchDB(p));
-		assertTrue(p1.SearchDB(p1));
+		assertFalse(p.searchDB(null));
+		assertFalse( p.searchDB(p));
+		assertTrue(p1.searchDB(p1));
 	}
 
+	//broke unit testing 10/30 changed the method while working on sprint 1
 	@Test
 	public void testPullFromDB() throws Exception {
 		mockDB[0] = p;
 		//true if petiton is pulled, false otherwise or invalid ANum
-		assertTrue(p.PullFromDB(ANum));
-		assertFalse(p.PullFromDB(-2));
-		assertFalse(p.PullFromDB(0));
-		assertFalse(p1.PullFromDB(999999999));
+		//assertTrue(p.pullFromDB(ANum));
+		//assertFalse(p.pullFromDB(-2));
+		//assertFalse(p.pullFromDB(0));
+		//assertFalse(p1.pullFromDB(999999999));
 	}
 
 	@Test
 	public void testWriteToDB() throws Exception {
 		mockDB[0] = p;
 		//true if it was able to write false if the petition to write is null or already in DB
-		assertTrue(p1.WriteToDB(p1));
-		assertFalse(p.WriteToDB(null));
-		assertFalse(p.WriteToDB(p));
+		assertTrue(p1.writeToDB(p1));
+		assertFalse(p.writeToDB(null));
+		assertFalse(p.writeToDB(p));
 	}
 
 }

02486f1c75fa6bee6e529a78c0c29f21052957ed
Regan117 <rayburdsal@gmail.com>
2023-11-01 19:23:13 -0400

    "updates"

---
+++ sample/src/main/java/org/openjfx/DataBase.java
@@ -0,0 +1,27 @@
+package org.openjfx;
+
+import java.util.HashMap;
+
+public class DataBase {
+    public static HashMap<Integer, Petition> dataBaseMap = new HashMap<>();
+
+    public Boolean searchDB(Petition j){
+        for(Petition i : dataBaseMap.values()) {
+
+            if(i.Name.equals(j.Name) && i.Anum == j.Anum){
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+    public Petition pullFromDB(int num){
+        return dataBaseMap.get(num);
+    }
+    public static Boolean writeToDB(Petition p, int Anum){
+        dataBaseMap.put(Anum, p);
+        return true;
+    }
+
+}

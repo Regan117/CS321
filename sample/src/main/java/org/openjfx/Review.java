3fab9d956a1e689b4d6c9c134da7f5a30b7cb0d1
Idkwhatshappenin <jacobmarion2@gmail.com>
2023-11-02 23:55:32 -0400

    Worked on Data Entry and saving to DataBase. Found that validation may be faulty.

---
+++ sample/src/main/java/org/openjfx/Review.java
@@ -0,0 +1,33 @@
+package org.openjfx;
+
+public class Review extends App {
+    protected String Name ;
+    protected String Email;
+    protected String DOB;
+    protected String Country;
+    protected int Anum;
+
+Review(String name, String email, String dob, String country, int anum){
+    this.Name = name;
+    this.Email = email;
+    this.DOB = dob;
+    this.Country = country;
+    this.Anum = anum;
+}
+
+    void Edit(){
+
+
+
+    }
+    boolean UpdateWF(){
+        return false;
+    }
+    void WriteToPetition(){
+        
+    }
+    void AccessPetition(){
+
+
+    }
+}
